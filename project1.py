6003d54418fb6f98025edcade7747179b2b50e1c
jellew16 <85550212+jellew16@users.noreply.github.com>
2021-10-03 19:53:24 +0200

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,25 @@
+What =input("/,*,+,-")
+Division = "/"
+Multiplication = "*"
+Addition = "+"
+Subtraction = "-"
+if What == Division:
+    num1 = float(input("Number 1:"))
+    num2 = float(input("Number 2:"))
+    som1 = num1 / num2
+    print(som1)
+if What == Multiplication:
+    num3 = float(input("Number 1:"))
+    num4 = float(input("Number 2:"))
+    som2 = num3 * num4
+    print(som2)
+if What == Addition:
+    num5 = float(input("Number 1:"))
+    num6 = float(input("Number 2:"))
+    som3 = num5 + num6
+    print(som3)
+if What == Subtraction:
+    num7 = float(input("Number 1:"))
+    num8 = float(input("Number 2:"))
+    som4 = num7 - num8
+    print(som4)
